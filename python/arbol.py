19b7555e7432462a695cdcf577dd775fcfa95448
David Montalvo <davidmontalvo211@gmail.com>
2020-12-26 16:43:10 -0500

    Add files via upload

---
+++ python/arbol.py
@@ -0,0 +1,16 @@
+altura = 10
+print((altura+1)*' '+'.·*·..·*·.·*·..·*·.')
+print((altura+2)*' '+' Feliz navidad (:')
+print((altura+1)*' '+'.·*·..·*·.·*·..·*·.')
+print(' ')
+
+for i in range(altura):
+    print((altura-i)*2*' '+'o'+ i*'~x~o')
+    print(((altura-i)*2-1)*' '+(i*2+1)*'/'+'|'+(i*2+1)*'\\')
+
+for leg in range(3):
+    print(('|||||').center(41))
+    
+print(('&&&&&&&&/=====\&&&&&&&&').center(41))
+
+    
